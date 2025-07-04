22251ff66911912b951340a01cbce0ff2d2a0beb
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2025-07-05 00:31:34 +0400

    Finalizing for work #4

--- pages/api/create-or-retrieve-customer.ts
+++ pages/api/create-or-retrieve-customer.ts
@@ -1,8 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
-const stripe = new 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", {
-  apiVersion: "2025-04-30.basil"});
+const stripe = new Stripe("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", { apiVersion: "2025-04-30.basil" });
 
 export default async function createOrRetriveCustomer(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
