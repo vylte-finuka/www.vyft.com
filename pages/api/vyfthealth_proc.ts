3fa143b6996b823f45124d4c70f38354cb300fd4
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2025-07-05 00:30:27 +0400

    Finalizing for work #3

--- pages/api/vyfthealth_proc.ts
+++ pages/api/vyfthealth_proc.ts
@@ -2,8 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 import clientPromise from "./lib/mongodb"; // Import pour accéder à MongoDB
 
-const stripe = new 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", {
-  apiVersion: "2025-04-30.basil"});
+const stripe = new Stripe("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", { apiVersion: "2025-04-30.basil" });
 
 type ResponseData = {
   success: boolean;
