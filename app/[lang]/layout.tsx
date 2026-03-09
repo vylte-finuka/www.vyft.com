2467d6b2d8bc46e467f2ec0fa0c8b98e71661b8f
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2026-03-09 22:58:26 +0400

    Updating layout.tsx.

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,45 +1,15 @@
-// app/[lang]/layout.tsx
-import type { Metadata } from "next";
-import type { ReactNode } from "react";
-import { Inter } from "next/font/google";
-import { GoogleAnalytics } from "@next/third-parties/google";
-import "../globals.css";
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const metadataContent = {
-  "fr-FR": {
-    title: "Vyft: La néobanque à la vertu de la finance.",
-    description: "La néobanque à la vertu de la finance.",
-  },
-  "en-EN": {
-    title: "Vyft: The neobank with the virtue of finance.",
-    description: "The neobank with the virtue of finance.",
-  },
-} as const;
-
-export async function generateMetadata({
-  params,
-}: {
-  params: { lang?: string };
-}): Promise<Metadata> {
-  const locale = params.lang === "en-EN" ? "en-EN" : "fr-FR";
-  return metadataContent[locale];
-}
-
-export default function RootLayout({
+export default function Layout({
   children,
   params,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
   params: { lang?: string };
 }) {
-  const locale = params.lang ?? "fr-FR"; // valeur par défaut si lang absent
-
   return (
-    <html lang={locale}>
-      <GoogleAnalytics gaId="G-TRM37NPSXN" />
-      <body className={inter.className}>{children}</body>
+    <html lang={params.lang}>
+      <body>{children}</body>
     </html>
   );
 }
