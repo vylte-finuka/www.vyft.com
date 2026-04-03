24cd9897ea2810b3f55c0ec7886ed1694845be5b
Vyftteam <123649969+vylte-finuka@users.noreply.github.com>
2026-04-03 20:32:32 +0400

    Updating VEZ icon source.

--- pages/[lang]/ecosystem/vyft-slura.tsx
+++ pages/[lang]/ecosystem/vyft-slura.tsx
@@ -129,7 +129,7 @@ export default function Vyftslide() {
           symbol: sluraCharene.nativeCurrency.symbol,
           decimals: sluraCharene.nativeCurrency.decimals,
           name: sluraCharene.nativeCurrency.name,
-          image: 'https://raw.githubusercontent.com/vylte-finuka/Slura/refs/heads/master/crates/vuc-platform/src/asset/Slura.png'
+          image: 'https://i.ibb.co/1YYrG08w/Slura.png'
         }
       };
 
